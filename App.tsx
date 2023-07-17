55ebd6365cb9db72afe9981bf5967b6d0436d897
Ivan da Cruz <64236894+ivandacruz@users.noreply.github.com>
2023-07-17 16:28:00 -0300

    feat: add Text Statistics

--- App.tsx
+++ App.tsx
@@ -1,10 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+type ValueProps = {
+  title: string;
+  label: string;
+  value: string;
+}
+
+const Value = ({ label, value, title }: ValueProps) => (
+  <View style={styles.valueContainer}>
+    <Text style={styles.title}>{title}</Text>
+    <Text style={styles.label}>{label}</Text>
+    <Text style={styles.value}>{value}</Text>
+  </View>
+);
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={{color:'red',}}>Open up App.tsx to start working on your app!</Text>
+
+      <View >
+        <Value title='Step Counter' />
+      </View>
+
+      <View style={{ flexDirection: 'row', }}>
+        <Value label='Steps' value="1219" />
+        <Value label='Distance' value="0,75 km" />
+      </View>
+
+      <Value label='Flights Climbed' value="12" />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +38,30 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#181818',
+    // alignItems: 'center',
     justifyContent: 'center',
+    padding: 13,
+    // margin right  =  margem direita
   },
+  title: {
+    color: 'gold',
+    fontSize: 33,
+    margin: 1,
+  },
+  valueContainer: {
+    marginRight: 50,
+  },
+  label: {
+    color: '#fff',
+    fontSize: 18,
+  },
+  value: {
+    color: 'green',
+    fontSize: 28,
+    // font weight = espessura da fonte
+    fontWeight: '500',
+
+  },
+
 });
--- README.md
+++ README.md
@@ -1,1 +1,31 @@
+<style type="text/css">
+    h1 {color: blue;}
+    h2 {color: green;}
+    h3 {color: red;}
+    
+    p { color: gold; }
+</style>
+
 # StepCounter
+
+Este app foi criado com intuito de fixar aprendizados e habilidades.
+
+## Descrição do projeto:
+
+- Aplicativo StepCounter em React Native. Conecta-se com Apple HealthKit e Android Health Connect.
+### Ingredientes do projeto
+
+- React native
+
+### O que foi aprendido
+
+- Integração de nosso aplicativo com o Apple Health e o Google Fit.
+- Integrando as APIs para sincronizar os dados do usuário.
+
+[![][black-shield]][black]
+
+[black]: http://github.com/psf/black
+[black-shield]: https://img.shields.io/badge/code%20style-black-black.svg?style=for-the-badge&labelColor=gray
+
+
+
